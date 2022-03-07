a8f5a1d88d4de87f9d02394892e12ad71cadd5ef
bezo16 <bezo16@protonmail.ch>
2022-03-07 12:18:33 +0100

    cards finished

--- pages/designers/index.vue
+++ pages/designers/index.vue
@@ -9,18 +9,18 @@
     </div>
     <section class="cards">
       <div v-for="(designer,index) in designers" :key="`designer${index}`" class="card">
-        <p>{{ designer.username }}</p>
-        <h2>{{ designer.name }}</h2>
-        <p>{{ designer.email }}</p>
+        <p class="card-username">{{ designer.username }}</p>
+        <h2 class="card-name">{{ designer.name }}</h2>
+        <p class="card-email">{{ designer.email }}</p>
         <div class="card-location">
-          <img src="" alt="" class="card-location--icon">
-          <p>{{ designer.address.street }} - {{ designer.address.suite }}</p>
-          <p>{{ designer.address.zipcode }} - {{ designer.address.city }}</p>
-          <p></p>
+          <img src="~/assets/img/location.svg" alt="" class="card-location--icon">
+          <p class="card-city">{{ designer.address.city }}</p>
+          <p class="card-street">{{ designer.address.street }} - {{ designer.address.suite }}</p>
+          <p class="card-address">{{ designer.address.zipcode }} - {{ designer.address.city }}</p>
         </div>
-        <a :href="designer.website">{{ designer.website }}</a>
+        <a class="card-website" :href="designer.website">{{ designer.website }}</a>
         <NuxtLink :to="`/designers/${index + 1}`">
-          <button class="card-button">ZOBRAZIT PROFIL</button>
+          <button class="card-button">ZOBRAZIT PROFIL --></button>
         </NuxtLink>
       </div>
     </section>
@@ -59,16 +59,69 @@ export default Vue.extend({
     display: flex
     flex-wrap: wrap
     justify-content: center
+    margin-top: 100px
     padding-bottom: 100px
 
   .card
-    width: 450px
-    margin: 20px
-    padding: 25px
+    width: 330px
+    margin: 15px
+    padding: 25px 25px 50px 25px
     border-radius: 25px
     background: white
+    display: flex
+    flex-direction: column
 
   .card-button
-    background-color: black
+    color: $primary
+    border: $primary 1.5px solid
+    border-top-right-radius: 30px
+    border-bottom-right-radius: 30px
+    border-top-left-radius: 30px
+    width: 250px
+    padding: 15px 20px
+    font-weight: bold
+
+  .card-button:hover
+    color: $secondary
+    background-color: $primary
+
+  .card-username
+    margin-bottom: 5px
+
+  .card-name
+    margin-bottom: 15px
+
+  .card-email
+    color: $grey
+    margin-bottom: 40px
+
+  .card-location
+    background-color: $secondary
+    border-radius: 20px
+    padding: 20px 45px 30px 45px
+    margin-bottom: 30px
+    position: relative
+
+  .card-location--icon
+    position: absolute
+    left: 15px
+    top: 20px
+
+  .card-city
+    font-weight: bold
+    margin-bottom: 10px
+
+  .card-street
+    font-size: 0.8rem
+    margin-bottom: 5px
+
+  .card-address
+    font-size: 0.8rem
+
+  .card-website
+    color: $primary
+    align-self: center
+    margin-bottom: 40px
+
 
 </style>
